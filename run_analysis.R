98d14012a061f0f0b5276beaea28097bddaa7e97
Elton de Freitas <eltondefreitas@outlook.com>
2015-12-26 21:42:48 -0200

    avrgs
    average

---
+++ run_analysis.R
@@ -0,0 +1,109 @@
+## Course project
+
+# set the directory (".\\" s the path of my machine)
+setwd(".\\coursera\\Getting and Cleaning Data")
+
+# Define the link download
+vlink <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+# Make the downnload
+if(!file.exists("./data")){dir.create("./data")}
+download.file(vlink,destfile="./data/TrainingAndTest.zip",method="auto")
+
+# Unzip the file
+if (file.exists("./data/TrainingAndTest.zip")) { 
+  unzip("./data/TrainingAndTest.zip",exdir = "./data") 
+}
+
+
+# Step 1
+# Merge the training and test sets to create one data set
+###############################################################################
+
+#read files
+
+# test
+x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+
+# train
+x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+
+# create 'x' data set (union all)
+x_data <- rbind(x_train, x_test)
+
+# create 'y' data set
+y_data <- rbind(y_train, y_test)
+
+# create 'subject' data set
+subject_data <- rbind(subject_train, subject_test)
+
+# Step 2
+# Extract only the measurements on the mean and standard deviation for each measurement
+###############################################################################
+
+# Read file
+features <- read.table("./data/UCI HAR Dataset/features.txt")
+
+# get only columns with mean() or std() in their names
+mean_and_std_features <- grep("-(mean|std)\\(\\)", features[, 2])
+
+# subset the desired columns
+x_data <- x_data[, mean_and_std_features]
+
+# correct the column names
+names(x_data) <- features[mean_and_std_features, 2]
+
+# Step 3
+# Use descriptive activity names to name the activities in the data set
+###############################################################################
+
+# read file
+activities <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
+
+# update values with correct activity names
+y_data[, 1] <- activities[y_data[, 1], 2]
+
+# correct column name
+names(y_data) <- "activity"
+
+
+# Step 4
+# Appropriately label the data set with descriptive variable names
+###############################################################################
+
+# correct column name
+names(subject_data) <- "subject"
+
+# bind all the data in a single data set
+all_data <- cbind(x_data, y_data, subject_data)
+
+# Step 5
+# Create a second, independent tidy data set with the average of each variable
+# for each activity and each subject
+###############################################################################
+
+# charge the library
+library(plyr)
+
+averages_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
+
+# create the file
+write.table(averages_data, "./data/UCI HAR Dataset/avrgs_data.txt", row.name=FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
